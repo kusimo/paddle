fd124b441002fc6b939acfc33993dd9b7cb01b29
Abidemi Kusimo <abidemi@MacBook-Pro.local>
2021-06-05 08:43:25 +0100

    version 1.0.11

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,6 +9,8 @@ import browserSync from "browser-sync";
 import zip from "gulp-zip";
 import info from "./package.json";
 import wpPot from "gulp-wp-pot";
+import concatCsss from "gulp-concat-css";
+import purgecss from "gulp-purgecss";
 
 const gulp = require('gulp');
 const babel = require('gulp-babel');
@@ -21,7 +23,7 @@ const server = browserSync.create();
 
 /** Style */
 export const styles = () => {
-    return src('src/scss/theme.scss')
+    return src('src/scss/main.scss')
       .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
       .pipe(sass().on('error', sass.logError))
       .pipe(gulpif(PRODUCTION,  autoprefixer( 'last 2 versions' )))
@@ -48,12 +50,34 @@ export const woo_styles = () => {
     .pipe(server.stream());
 }
 
+
+
+/** Bootstrap Purge */
+
+export const purge_bootstrap = () => {
+  return src('css/bootstrap.css')
+  .pipe(gulpif(PRODUCTION,  autoprefixer( 'last 2 versions' )))
+    //purge
+    .pipe(purgecss({
+      content: ['**/*.php']
+  }))
+  .pipe(gulpif(PRODUCTION,  rename( { 
+      basename: "custom-bootstrap",
+    } ) ))
+  .pipe(dest('css/'))
+  .pipe(gulpif(PRODUCTION, cleanCss({compatibility:'ie8'})))
+  .pipe(gulpif(PRODUCTION,  rename( { 
+  //  basename: "custom-bootstrap",
+    suffix: '.min' 
+  } ) ))
+  .pipe(gulp.dest('css/'))
+}
+
 /** Script */
 export const scripts = () => {
     return src([
-      'src/js/theme.js',
-      'src/js/navigation.js',
-      'src/js/logo-script.js'
+      'src/js/main.js',
+      'src/js/navigation.js'
     ])
     .pipe(dest('js/'))
     .pipe(gulpif(PRODUCTION, 
@@ -87,9 +111,8 @@ export const scripts = () => {
    // CSS to RTL.
   export const compilertl = () => {
     return src([
-      'css/theme.css',
-      'css/woocommerce.css',
-      './style.css'
+      'css/main.css',
+      'css/woocommerce.css'
       ])
      .pipe(rtlcss())
      .pipe(rename( { suffix: '-rtl' } ))
@@ -122,6 +145,10 @@ export const scripts = () => {
         "!composer.json",
         "!composer.lock",
         "!package-lock.json",
+        "!.DS_Store",
+        "!.stylelintrc.json",
+        "!.eslintrc",
+        "!.git",
       ])
       .pipe(zip(`${info.name}.zip`))
       .pipe(dest('bundled'));
@@ -143,3 +170,13 @@ export const pot = () => {
 export const dev = series(parallel(styles, woo_styles, scripts, images), serve, watchFor)
 export const build = series(parallel(styles, woo_styles, scripts, images), compress) // issue with pot
 export default dev;
+
+/*
+gulp styles --prod
+gulp scripts --prod
+gulp woo_styles --prod
+gulp compress
+gulp rtl
+gulp dev",
+gulp build"
+*/
